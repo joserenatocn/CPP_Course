b43ca7d87b81cfc327284ee822fc98e44c3a2d63
joserenatocn <jjrenatocn@gmail.com>
2025-11-18 11:16:07 -0300

    Initial Commit

---
+++ CPP_Course/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CPP_Course
+//
+//  Created by Jose Renato da Costa Nobrega on 11/18/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
